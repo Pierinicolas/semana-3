091b2ac0cf3258e6e0fb9642a3abd96e79a9c0a7
nico <nico@DESKTOP-T9MBI0Q>
2024-04-11 03:04:10 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace semana_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
